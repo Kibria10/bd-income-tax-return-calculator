77fcc597afb4f3965e9c465c8df953df50e8332f
Maharab Kibria <junakkibria56@gmail.com>
2023-01-31 13:35:01 +0600

    UI updated and bugfixed for car benefits

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
       gender: '',
       basicSalary: '',
       hra: '',
+      med: '',
       conveyance: '',
       festivalFund: '',
       performance: '',
@@ -24,74 +25,92 @@ class App extends Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
-    const { gender, basicSalary, hra, conveyance, festivalFund, performance, carBenefit } = this.state;
-    let totalAmount = parseFloat(basicSalary) + parseFloat(hra) + parseFloat(conveyance) + parseFloat(festivalFund) + parseFloat(performance);
+    const { gender, basicSalary, hra, med, conveyance, festivalFund, performance, carBenefit } = this.state;
+    let totalAmountPerMonth = parseFloat(basicSalary) + parseFloat(hra) + parseFloat(med) + parseFloat(conveyance);
+    let totalAmount = totalAmountPerMonth * 12 + parseFloat(festivalFund) + parseFloat(performance);
     this.setState({ taxAmount: this.calculateTax(totalAmount, gender, carBenefit, basicSalary) });
   };
-  
+
   calculateTax = (totalAmount, gender, carBenefit, basicSalary) => {
     //income tax calculation based on gender and car benefit
     let carBenefitTax = 0;
-    if(gender === 'Men' && carBenefit === 'Enabled') {
-      carBenefitTax = basicSalary * 0.05;
+    if (carBenefit === 'true') {
+      carBenefitTax = basicSalary * 12 * 0.05; // 5% of yearly basic salary
     }
 
-    if(gender === 'Men' && carBenefit === 'Disabled' || gender === 'Men' && carBenefit === '') {
-      if(totalAmount <= 300000){
+    if (gender === 'Men') {
+      if (totalAmount <= 300000) {
         return carBenefitTax;
-      }else if(totalAmount >= 300000 && totalAmount <= 400000){
+      } else if (totalAmount >= 300000 && totalAmount <= 400000) {
         return carBenefitTax + (totalAmount - 300000) * 0.05;
-      }else if(totalAmount >= 400000 && totalAmount <= 700000){
+      } else if (totalAmount >= 400000 && totalAmount <= 700000) {
         return carBenefitTax + (totalAmount - 400000) * 0.1 + 5000;
-      }else{
+      } else {
         return carBenefitTax + (totalAmount - 700000) * 0.15 + 35000;
       }
+    }
+
+    //for women the there is no tax for first 450000, then 5% for next 100000, 10% for next 300000 and 15% for rest
+    else if (gender === 'Women' || gender === 'TG') {
+      if(totalAmount <= 450000) {
+        return carBenefitTax;
+      }
+      else if(totalAmount >= 450000 && totalAmount <= 550000) {
+        return carBenefitTax + (totalAmount - 450000) * 0.05;
+      }
+      else if(totalAmount >= 550000 && totalAmount <= 850000) {
+        return carBenefitTax + (totalAmount - 550000) * 0.1 + 5000;
+      }
+      else {
+        return carBenefitTax + (totalAmount - 850000) * 0.15 + 35000;
+      }
+      
     }
   };
 
   render() {
     return (
-      <div className="app" style={{ width: '100%', height: '100%' }}>
-        <header>
-          <h1>Bangladesh Income Tax Return Calculator for Salary</h1>
-        </header>
-        <main>
-          <form onSubmit={this.handleSubmit}>
+      <div className="App">
+        <div className="particles-background" />
+        <p> Bangladesh Income Tax Return Calculator for Job Holders (NBR:2022-23)</p>
+        <form onSubmit={this.handleSubmit} className="form">
           <label>
-              <select name="gender" onChange={this.handleInputChange}>
-                <option value="">Select Gender</option>
-                <option value="Men">Men</option>
-                <option value="Women">Women</option>
-                <option value="Third Gender">Third Gender</option>
-              </select>
-            </label> 
-            <label>
-              <input type="text" name="basicSalary" placeholder='Basic Salary' onChange={this.handleInputChange} />
-            </label> 
-            <label>
-              <input type="text" name="hra" placeholder='House Rent Allowance' onChange={this.handleInputChange} />
-            </label> 
-            <label>
-              <input type="text" name="conveyance" placeholder='Conveyance Allowance' onChange={this.handleInputChange} />
-            </label> 
-            <label>
-              <input type="text" name="festivalFund" placeholder='Festival Fund Amount' onChange={this.handleInputChange} />
-            </label> 
-            <label>
-              <input type="text" name="performance" placeholder='Performance Bonus' onChange={this.handleInputChange} />
-            </label> 
-            <label>
-              <select name="gender" onChange={this.handleInputChange}>
-                <option value="">Select Car Benefit</option>
-                <option value="Enabled">Enabled</option>
-                <option value="Disabled">Disabled</option>
-              </select>
-            </label> 
-            <br />
-            <button type="submit" className="calculate-button">Calculate Tax</button>
-          </form>
-          <p className="tax-amount">Your Income tax: {this.state.taxAmount}</p>
-        </main>
+            <select name="gender" onChange={this.handleInputChange}>
+              <option value="">Select Gender</option>
+              <option value="Men">Men</option>
+              <option value="Women">Women</option>
+              <option value="TG">Third Gender</option>
+            </select>
+          </label>
+          <label>
+            <input type="number" name="basicSalary" placeholder='Basic Salary Per Month (৳)' onChange={this.handleInputChange} />
+          </label>
+          <label>
+            <input type="number" name="hra" placeholder='House Rent Allowance Per Month (৳)' onChange={this.handleInputChange} />
+          </label>
+          <label>
+            <input type="number" name="med" placeholder='Medical Allowance Per Month (৳)' onChange={this.handleInputChange} />
+          </label>
+          <label>
+            <input type="number" name="conveyance" placeholder='Conveyance Allowance Per Month (৳)' onChange={this.handleInputChange} />
+          </label>
+          <label>
+            <input type="number" name="festivalFund" placeholder='Total Festival Bonus (৳)' onChange={this.handleInputChange} />
+          </label>
+          <label>
+            <input type="number" name="performance" placeholder='Total Performance Bonus (৳)' onChange={this.handleInputChange} />
+          </label>
+          <label>
+            <select name="carBenefit" onChange={this.handleInputChange}>
+              <option value="">Select Car Benefit</option>
+              <option value="true">Enabled</option>
+              <option value="false">Disabled</option>
+            </select>
+          </label>
+          <br />
+          <button type="submit" className="calculate-button">Calculate Tax</button>
+        </form>
+        <p className="tax-amount">Your Income tax is {this.state.taxAmount} ৳</p>
       </div>
     );
   }
